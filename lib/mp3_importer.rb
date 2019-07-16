32a376fdd92809e1e955e6efab7699a6f59bb028
Alicia Fasciocco <afasciocco@gmail.com>
2019-07-16 07:41:10 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,14 @@
+class MP3Importer
+  attr_accessor :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    files = Dir.glob()
+    
+  def import(list_of_filenames)
+    files.each{ |file| Song.new_by_filename(file) }
+  end
+end
